fea7e922ace175d7b2fc8b770111927be849bbcc
+ <+>
2022-05-23 17:41:50 -0400

    commit tree 4f60e7865cf09174fb5190fd03765391306ed292

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+const giftCard = []
+
+function writeCards(gifts) {
+  
+    for (let i = 0; i < gifts.length; i++) {
+
+     giftCard[i] = `Thank you, ${gifts[i]}, for the wonderful surprise gift!`;
+
+    };
+
+return giftCard;
+
+}
+
+function countDown(count) {
+    for (let i = count; count >= 0 ;count-1) {
+        console.log(count--);
+    }
+
+}
+
